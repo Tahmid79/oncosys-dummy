476b39f5902063875036a72b46798ba02c4af2cd
Tahmid Farhan <tahmidfarhan12@gmail.com>
2021-11-18 18:17:31 +0600

    form field components

---
+++ src/app/contact/form-field/form-field.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-form-field',
+  templateUrl: './form-field.component.html',
+  styleUrls: ['./form-field.component.scss']
+})
+export class FormFieldComponent implements OnInit {
+  @Input() label: string = '' ;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
